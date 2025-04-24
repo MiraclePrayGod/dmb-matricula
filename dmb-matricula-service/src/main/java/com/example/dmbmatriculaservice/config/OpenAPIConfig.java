fd9e6b77af3eb9df46d81d912b7a7fbdc59211ce
MiraclePrayGod <reginaldobuendia@gmail.com>
2025-04-23 21:46:27 -0500

    vamosssssssss

--- dmb-matricula-service/src/main/java/com/example/dmbmatriculaservice/config/OpenAPIConfig.java
+++ dmb-matricula-service/src/main/java/com/example/dmbmatriculaservice/config/OpenAPIConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class customOpenAPI {
+public class OpenAPIConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
--- dmb-matricula-service/src/main/java/com/example/dmbmatriculaservice/entity/Matricula.java
+++ dmb-matricula-service/src/main/java/com/example/dmbmatriculaservice/entity/Matricula.java
@@ -1,10 +1,10 @@
 package com.example.dmbmatriculaservice.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-import java.awt.*;
-@Data
 @Entity
 public class Matricula {
     @Id
@@ -12,4 +12,27 @@ public class Matricula {
     private Integer id;
     private String course;
 
+    public Matricula() {
+    }
+
+    public Matricula(Integer id, String course) {
+        this.id = id;
+        this.course = course;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
 }
